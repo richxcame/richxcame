bb16775055ac5ca72f1ba072d22eafc4caf6a7db
Baygeldi Cholukov <baygeldicolukov@gmail.com>
2022-01-20 21:10:26 +0500

    doc: add about me

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-import chalk from "chalk";
-const log = console.log;
+import chalk from 'chalk';
+import boxen from 'boxen';
 
-log("Hi, My name is " + chalk.blue("Baygeldi"));
+const { log } = console;
+
+log(boxen('richxcame', { borderStyle: 'arrow', margin: 2, padding: 1 }));
+
+log(
+	'Hi 👋, My name is ' +
+		chalk.bold.underline.blue('Baygeldi') +
+		'. I ❤️  111(☘️ ) and VIM'
+);
+
+log(`
+About me:
+  ☕️  Coffee geek
+  🥷  Coding ninja
+  🎧  Music enthusiast
+  🧠  Math lover
+`);
+
+log(`
+Contacts:
+  Email:  baygeldicholukov@gmail.com
+  Phone:  +993 62726535
+  Github: richxcame
+`);
